feb5e26f7eab98691cb3ab4c8ec927c15e8ceef2
vaibhav <vaibhav@LAPTOP-DUMLSALK>
2024-01-31 22:17:12 +0530

    remaining

---
+++ MaverickBankAPI/Models/Administrator.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MaverickBankAPI.Models
+{
+    /// <summary>
+    /// Represents an administrator entity.
+    /// </summary>
+    public class Administrator
+    {
+        /// <summary>
+        /// Gets or sets the administrator ID.
+        /// </summary>
+        [Key]
+        public int AdministratorID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the associated user ID.
+        /// </summary>
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the associated user.
+        /// </summary>
+        public User User { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the administrator.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
---
+++ MaverickBankAPI/Models/Loan.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MaverickBankAPI.Models
+{
+    /// <summary>
+    /// Represents a loan entity.
+    /// </summary>
+    public class Loan
+    {
+        /// <summary>
+        /// Gets or sets the loan ID.
+        /// </summary>
+        [Key]
+        public int LoanID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the associated customer ID.
+        /// </summary>
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the associated customer.
+        /// </summary>
+        public Customer Customer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the loan amount.
+        /// </summary>
+        public float LoanAmount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the interest rate.
+        /// </summary>
+        public float InterestRate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tenure in months.
+        /// </summary>
+        public int TenureMonths { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the loan.
+        /// </summary>
+        public string Status { get; set; }
+    }
+}
